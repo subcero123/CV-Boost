76b0e05aa4309dcfd2a7b084ad6abad146dacb09
subcero123 <subcero139@hotmail.com>
2023-10-25 22:16:35 -0600

    Entorno flask agregado

--- chatgpt/chatgpt.py
+++ chatgpt/chatgpt.py
@@ -1,10 +1,10 @@
-
+from flask import Flask, request, render_template
 import openai
-import os
+
+app = Flask(__name__)
 
 # Clave de la API de OpenAI
-openai.api_key = ""
-
+openai.api_key = "TU_CLAVE_DE_API_DE_OPENAI"
 
 # Función para obtener sugerencias de mejoras en una sección del CV
 def obtener_sugerencias(texto):
@@ -33,19 +33,40 @@ def obtener_sugerencias_para_secciones(cv):
     
     return sugerencias_por_seccion
 
-# Datos de ejemplo del CV
-cv = {
-    "Educación": "Soy estudiante de Ciencias de la Computación en la Universidad XYZ.",
-    "Experiencia Laboral en Tecnología": "",
-    "Habilidades Técnicas": "Tengo experiencia en Python, Java, Linux, bases de datos SQL y herramientas de desarrollo como Git.",
-    "Proyectos Personales o de Grupo": "",
-    "Hackathons y Competencias Técnicas": "Gané el primer lugar en el Hackathon ABC en 2022.",
-    "Proyectos de Código Abierto": "",
-}
 
-# Llama a la función para obtener sugerencias para secciones específicas
-sugerencias_por_seccion = obtener_sugerencias_para_secciones(cv)
 
-# Imprime las sugerencias para cada sección no vacía
-for seccion, sugerencias in sugerencias_por_seccion.items():
-    print(f"Sugerencias para la sección '{seccion}':\n{sugerencias}\n")
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/obtener_sugerencias', methods=['POST'])
+def obtener_sugerencias_endpoint():
+    try:
+        # Datos de ejemplo del CV
+        cv = {
+            "Educación": "Soy estudiante de Ciencias de la Computación en la Universidad XYZ.",
+            "Experiencia Laboral en Tecnología": "",
+            "Habilidades Técnicas": "Tengo experiencia en Python, Java, Linux, bases de datos SQL y herramientas de desarrollo como Git.",
+            "Proyectos Personales o de Grupo": "",
+            "Hackathons y Competencias Técnicas": "Gané el primer lugar en el Hackathon ABC en 2022.",
+            "Proyectos de Código Abierto": "",
+        }
+        sugerencias_por_seccion = obtener_sugerencias_para_secciones({"Educación": cv})
+        return render_template('index.html', cv_sugerencias=sugerencias_por_seccion)
+    except Exception as e:
+        return render_template('index.html', error=str(e))
+
+if __name__ == '__main__':
+    app.run(debug=True)
